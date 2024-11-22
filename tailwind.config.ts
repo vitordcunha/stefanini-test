b4e18ae4a75f71317d1bdbbb11c2dee7ce880f46
vitor852 <vitor.dc@hotmail.com>
2024-11-22 17:37:22 -0300

    chore: add header css vars

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        header: {
+          background: "var(--header-background)",
+          foreground: "var(--header-foreground)",
+          border: "var(--header-border)",
+        },
       },
     },
   },
