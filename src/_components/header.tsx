613a7f0f235442fe8b0e60972f64dad9a36983f0
vitor852 <vitor.dc@hotmail.com>
2024-11-22 19:28:12 -0300

    fix: header bg color name

--- src/_components/header.tsx
+++ src/_components/header.tsx
@@ -3,7 +3,7 @@ import HeaderLink from "./header-link";
 const Header = () => {
   return (
     <div className="p-5">
-      <header className="rounded-lg bg-header-background border border-header-border shadow-lg p-5">
+      <header className="rounded-lg bg-card-background border border-border shadow-lg p-5">
         <nav>
           <ul className="flex items-center gap-5">
             <li>
