8afd93a3066cdab6ab5f8f8836da25b3748a7a03
vitor852 <vitor.dc@hotmail.com>
2024-11-22 21:44:56 -0300

    fix: modify input to generic component

--- src/_components/input.tsx
+++ src/_components/input.tsx
@@ -1,28 +1,16 @@
-export interface InputProps extends React.HTMLProps<HTMLInputElement> {
-  label: string;
-  name: string;
-}
-
-const Input = ({ label, ...props }: InputProps) => {
+const Input = (props: React.HTMLProps<HTMLInputElement>) => {
   return (
-    <div className="container flex flex-col items-start gap-2">
-      <label htmlFor={props.id} className="text-card-foreground font-light">
-        {label}
-      </label>
-
-      <input
-        type="text"
-        id={props.name}
-        {...props}
-        className="
-          bg-input-background p-3 w-full transition-all outline-none
+    <input
+      type="text"
+      id={props.name}
+      {...props}
+      className="
+          bg-input-background p-3 w-full max-w-[400px] transition-all outline-none
           border rounded-lg border-input-background focus:border-input-focus-border
           placeholder-card-foreground placeholder:font-light
         "
-        placeholder={label}
-        autoComplete="off"
-      />
-    </div>
+      autoComplete="off"
+    />
   );
 };
 
