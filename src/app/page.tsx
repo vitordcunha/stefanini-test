db5db24fc1096f31a5cd5a3315f99286a682623e
vitor852 <vitor.dc@hotmail.com>
2024-11-22 21:45:52 -0300

    style: home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import SearchForm from "@/_components/search-form";
 
 export default function Home() {
   return (
-    <div className="flex h-full w-full justify-center items-center">
-      <div className="bg-card-background p-10 border border-border rounded-lg w-[500px]">
-        <h1 className="text-xl mb-6">Buscar filme</h1>
-        <SearchForm />
-      </div>
+    <div className="flex flex-col w-[400px]">
+      <h1 className="text-xl mb-6">Buscar filme</h1>
+      <SearchForm />
     </div>
   );
 }
